200bf89af75fcf89f2179b03bef90841399a5a99
seanphenomenon <seanvharrell@gmail.com>
2020-10-11 18:20:14 -0700

    added react-flippy

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -9,6 +9,8 @@ import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import Grid from "@material-ui/core/Grid";
 
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+
 const useStyles = makeStyles((theme) => ({
   letters: {
     fontFamily: "Comic+Neue",
@@ -26,48 +28,51 @@ export default function Flashcard() {
   const classes = useStyles();
   const theme = useTheme();
 
-
   const handleNextClick = () => {
-      console.log('you clicked the next button!')
-  }
+    console.log("you clicked the next button!");
+  };
 
   const handleBackClick = () => {
-      console.log('you clicked the back button!')
-  }
+    console.log("you clicked the back button!");
+  };
 
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h1" align="center" className={classes.letters}>
-          Aa
-        </Typography>
-        <Typography
-          variant="h2"
-          align="center"
-          style={{ marginTop: "1.5em", marginBottom: "1em" }}
-        >
-          Array
-        </Typography>
-        <CardActions>
-          <Grid
-            container
-            justify="space-evenly"
-            alignItems="center"
-            className={classes.buttonContainer}
-          >
-            <Grid item>
-              <Button onClick={handleBackClick}>
-                <ArrowBackIosIcon fontSize="large" />
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button onClick={handleNextClick}>
-                <ArrowForwardIosIcon fontSize="large" />
-              </Button>
-            </Grid>
-          </Grid>
-        </CardActions>
-      </CardContent>
-    </Card>
+    <Flippy flipOnHover={false} flipOnClick={true} flipDirection="horizontal">
+      <FrontSide>
+        <Card>
+          <CardContent>
+            <Typography variant="h1" align="center" className={classes.letters}>
+              Aa
+            </Typography>
+            <Typography
+              variant="h2"
+              align="center"
+              style={{ marginTop: "1.5em", marginBottom: "1em" }}
+            >
+              Array
+            </Typography>
+            <CardActions>
+              <Grid
+                container
+                justify="space-evenly"
+                alignItems="center"
+                className={classes.buttonContainer}
+              >
+                <Grid item>
+                  <Button onClick={handleBackClick}>
+                    <ArrowBackIosIcon fontSize="large" />
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button onClick={handleNextClick}>
+                    <ArrowForwardIosIcon fontSize="large" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </FrontSide>
+    </Flippy>
   );
 }
